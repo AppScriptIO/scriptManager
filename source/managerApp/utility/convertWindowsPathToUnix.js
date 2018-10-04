75cf6faf808b437bf38fe36d46dd4c90be16f4f9
Dr. Safi <sfmissive@gmail.com>
2018-10-04 08:31:00 +0300

    🐛 Windows host machine path

--- source/managerApp/utility/convertWindowsPathToUnix.js
+++ source/managerApp/utility/convertWindowsPathToUnix.js
@@ -5,6 +5,6 @@ export function convertWindowsPathToUnix({ path }) {
     path = upath.normalize(path) // convert slashes to Unix.
     let driveLetterArray = windowsDriveLetterRegex.exec(path),
         driveLetter = (driveLetterArray) ? driveLetterArray[0] : false;
-    path =  path.replace(driveLetter, `/${driveLetter.slice(0, 1)}`) // remove ':' from drive letter and add a slash at the beginning.
+    path = (driveLetter) ?  path.replace(driveLetter, `/${driveLetter.slice(0, 1)}`) : path; // remove ':' from drive letter and add a slash at the beginning.
     return path
 }
