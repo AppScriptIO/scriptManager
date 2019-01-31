54af92340b8bbf01c4e17e949f5e7b9631017d3b
Dr. Safi <sfmissive@gmail.com>
2019-01-31 13:21:52 +0200

    🐛 Fix condition check  for `array.indexOf` where -1 should be boolean

--- source/scriptManager/utility/splitArray.js
+++ source/scriptManager/utility/splitArray.js
@@ -1,9 +1,8 @@
-
 // divide an array into two separate arrays by an item value that equals the provided delimiter
 export function splitArrayToTwoByDelimiter({ array, delimiter }) {
     let beforeDelimiter = [], afterDelimiter = [];
     let delimiterIndex = array.indexOf(delimiter)
-    if(delimiterIndex){
+    if(delimiterIndex >= 0 ){
         beforeDelimiter = array.slice(0, delimiterIndex)
         afterDelimiter = array.slice(delimiterIndex + 1)
     } else 
