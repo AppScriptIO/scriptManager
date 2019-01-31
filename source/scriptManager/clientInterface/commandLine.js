65aee23faa8e899eedf93c9b1ff7bdf9416c43ad
Dr. Safi <sfmissive@gmail.com>
2019-01-31 13:25:41 +0200

    ⚗ Simplify code and remove duplicates for 2 cli modes of operation.

--- source/scriptManager/clientInterface/commandLine.js
+++ source/scriptManager/clientInterface/commandLine.js
@@ -21,60 +21,64 @@ import { loadStdin } from "../utility/loadStdin.js"
 import { isJSCodeToEvaluate } from '../utility/isJSCodeToEvaluate.js'
 import { splitArrayToTwoByDelimiter } from "../utility/splitArray.js"
 
-// choose mode of the cli execution depending on the command line arguments
-;(function() {
+cliInterface().catch(error => { console.error(error) })
 
-  let firstCommandArgument = process.argv[2]
-  // check if the first argument for is a Javascript code that should be evaluated on an imported module.
-  if(isJSCodeToEvaluate({ string: firstCommandArgument })) 
-    cliInterfaceEvaluate().catch(error => console.error(error))  
-  else 
-    cliInterface().catch(error => console.error(error))
-})()
-
-/**
- * This implementation, in contrast to the other cliInterfaceEvaluate, requires mapping the needed commandline parsed arguments to the method parameters.
- * distinguish between the ownConfiguration and the target application configuration.
- * USAGE: 
- *  script invokation from shell using: npx || yarn run || <pathToScript e.g. './node_modules/.bin/scriptManager'>   (`yarn run` is prefered over `npx` because it correctly catches errors, i.e. its implementation is more complete.)
- *  $ `yarn run scriptManager targetAppConfigPath=<> scriptKeyToInvoke=<filename> jsCodeToEvaluate=<js code> -- <arguments passed to target script>`
- *  where `--` means the end of own module args and beginning of target script args (a slightlt different meaning than the convention in other shell scripts https://serverfault.com/questions/114897/what-does-double-dash-mean-in-this-shell-command).
- *  shorthand $ `yarn run scriptManager <scriptToInvoke> <jsCodeToEvaluate>` e.g. `yarn run scriptManager sleep '.setInterval()'`
+/** 
+ * Could run in two modes: 
+ *  1. Evaluate code interface: Allows for calling this module `scriptManager` using javasript code from the commandline.
+ *     In this case there are no parsed command arguments, only the first argument that contains JS code with all necessary parameters.
+ *     USAGE: 
+ *       `yarn run scriptManager "({ scriptKeyToInvoke: 'sleep' })"`
+ *       `yarn run scriptManager "({ scriptKeyToInvoke: 'sleep', jsCodeToEvaluate: '.setInterval()' })"`
+ *  2. parsed arguments interface: This implementation, in contrast to the other code evaluation interface, requires mapping the needed commandline parsed arguments to the method parameters.
+ *     USAGE: 
+ *      script invokation from shell using: npx || yarn run || <pathToScript e.g. './node_modules/.bin/scriptManager'>   (`yarn run` is prefered over `npx` because it correctly catches errors, i.e. its implementation is more complete.)
+ *      $ `yarn run scriptManager targetAppConfigPath=<> scriptKeyToInvoke=<filename> jsCodeToEvaluate=<js code> -- <arguments passed to target script>`
+ *      where `--` means the end of own module args and beginning of target script args (a slightlt different meaning than the convention in other shell scripts https://serverfault.com/questions/114897/what-does-double-dash-mean-in-this-shell-command).
+ *      shorthand $ `yarn run scriptManager <scriptToInvoke> <jsCodeToEvaluate> -- <arguments to target script>` e.g. `yarn run scriptManager sleep '.setInterval()'`
+ * 
+ * [note] distinguish between the ownConfiguration and the target application configuration.
  */
 async function cliInterface({
-  // key value pair object representing the passed values.
   envrironmentArgument = process.env,
   commandArgument = process.argv.slice(2),
   currentDirectory = process.env.PWD, 
   scriptKeyToInvoke, // the key name for the script that should be executed (compared with the key in the configuration file.)
-  jsCodeToEvaluate, // javascript code to evaluate on target script. 
   targetAppConfigPath, // the path to the configuration file of the target application. relative path to target project configuration from current working directory.
   argumentDelimiter = '--', // delimiter symbol for differentiating own arguments from the target script arguments.
-} = {}) {
-  console.log(`\x1b[33m\x1b[1m\x1b[7m\x1b[36m%s\x1b[0m \x1b[2m\x1b[3m%s\x1b[0m`, `Script:`, `NodeJS App`)
+  jsCodeToEvaluate
+} = []) {
 
-  /** Argument initialization, validation, sanitization */
-  let standartInputData = await loadStdin() // in case in shell pipeline - get input
-  // split array by `--` delimiter
-  let [ownCommandArgument, targetScriptCommandArgument] = splitArrayToTwoByDelimiter({ array: commandArgument, delimiter: argumentDelimiter })
-  let parsedCommandArgument = parseKeyValuePairSeparatedBySymbolFromArray({ array: ownCommandArgument }) // parse `key=value` node command line arguments
-
-  /**
+  /** Argument initialization, validation, sanitization 
    * get arguments - API of accepted varibales from (priority list)
    * 1. immediately passed argument in code. 
-   * 2. container passed environment variables
-   * 3. CLI arguments
-   */
-  scriptKeyToInvoke = scriptKeyToInvoke || envrironmentArgument.scriptKeyToInvoke || parsedCommandArgument.scriptKeyToInvoke 
-                        || ownCommandArgument[0]   // allow for shorthand command call.
-  jsCodeToEvaluate = parsedCommandArgument.jsCodeToEvaluate 
-                      || ownCommandArgument[1]   // allow for shorthand command call.
+   * 2. Environment variables
+   * 3. Commandline arguments
+  */
+  let standartInputData = await loadStdin() // in case in shell pipeline - get input
+  // split commandline arguments by delimiter
+  let [ownCommandArgument, targetScriptCommandArgument] = splitArrayToTwoByDelimiter({ array: commandArgument, delimiter: argumentDelimiter })
+  let parsedCommandArgument = parseKeyValuePairSeparatedBySymbolFromArray({ array: ownCommandArgument }) // parse `key=value` node command line arguments
+  // create command arguments for target script. 
+  process.argv = [process.argv[0], process.argv[1] /* should be substituted by full target script path after lookup */, ...targetScriptCommandArgument] 
 
-  // target application's configuration file parameter hierarchy
-  targetAppConfigPath = targetAppConfigPath || parsedCommandArgument.targetConfig || standartInputData /* stdin input */ || envrironmentArgument.targetConfig
+  console.log(ownCommandArgument)
+  const isEvaluateCodeInterface = isJSCodeToEvaluate({ string: ownCommandArgument[0] })
 
-  // create command arguments for target script. The path to the script should be changed after script lookup by succeeding modules.
-  process.argv = [process.argv[0], `${scriptKeyToInvoke}` /* should be substituted by full target script path after lookup */, ...targetScriptCommandArgument] 
+  if(isEvaluateCodeInterface) {
+    targetAppConfigPath = targetAppConfigPath || standartInputData || envrironmentArgument.targetConfig
+    scriptKeyToInvoke = scriptKeyToInvoke || envrironmentArgument.scriptKeyToInvoke  
+  } else {
+    scriptKeyToInvoke = scriptKeyToInvoke || parsedCommandArgument.scriptKeyToInvoke || envrironmentArgument.scriptKeyToInvoke 
+                          || ownCommandArgument[0]   // allow for shorthand command call.
+    jsCodeToEvaluate = jsCodeToEvaluate || parsedCommandArgument.jsCodeToEvaluate || envrironmentArgument.scriptKeyToInvoke
+                        || ownCommandArgument[1]   // allow for shorthand command call.
+  
+    // target application's configuration file parameter hierarchy
+    targetAppConfigPath = targetAppConfigPath || parsedCommandArgument.targetConfig || standartInputData /* stdin input */ || envrironmentArgument.targetConfig
+  
+    process.argv[1] = scriptKeyToInvoke || process.argv[1] //The path to the script should be changed after script lookup by succeeding modules.
+  }  
 
   // target application configuration file:
   ;({ path: targetAppConfigPath } = configurationFileLookup({
@@ -85,53 +89,33 @@ async function cliInterface({
   // assret entrypoint configuration objects/options exist.
   console.assert(require.resolve(targetAppConfigPath), '\x1b[41m%s\x1b[0m', `❌ Configuration file doesn't exist in ${targetAppConfigPath}`)
 
-  await scriptManager({
-    targetAppConfigPath,
-    scriptKeyToInvoke, 
-    jsCodeToEvaluate, 
-  }).catch(error => { console.error(error) })
-
+  // check if the first argument for is a Javascript code that should be evaluated on an imported module.
+  if(isEvaluateCodeInterface) 
+    evaluateCodeInterface({
+        codeToEvaluateForOwnModule: ownCommandArgument[0], 
+        defaultEvaluateCallValueForFirstParameter: { targetAppConfigPath, scriptKeyToInvoke, jsCodeToEvaluate }
+    }).catch(error => console.error(error))  
+  else 
+    await scriptManager({
+      targetAppConfigPath,
+      scriptKeyToInvoke, 
+      jsCodeToEvaluate, 
+    }).catch(error => { console.error(error) })
 }
 
-/** 
- * Allows for calling this module `scriptManager` using javasript code from the commandline.
- * In this case there are no parsed command arguments, only the first argument that contains JS code with all necessary parameters.
- * Example: 
- *  `yarn run scriptManager "({ scriptKeyToInvoke: 'sleep' })"`
- *  `yarn run scriptManager "({ scriptKeyToInvoke: 'sleep', jsCodeToEvaluate: '.setInterval()' })"`
- */
-async function cliInterfaceEvaluate({
-  // key value pair object representing the passed values.
-  scriptKeyToInvoke, // the key name for the script that should be executed (compared with the key in the configuration file.)
-  targetAppConfigPath, // the path to the configuration file of the target application.
-  currentDirectory = process.env.PWD, 
-  codeToEvaluateForOwnModule = process.argv[2],
-  envrironmentArgument = process.env,
+// execute api using string evaluated code.
+async function evaluateCodeInterface({
+  codeToEvaluateForOwnModule, // string js code that will be used on the callback.
+  defaultEvaluateCallValueForFirstParameter = {},
+  callback = scriptManager
 } = {}) {
-
-  let standartInputData = await loadStdin() // in case in shell pipeline - get input
-  targetAppConfigPath = targetAppConfigPath || standartInputData || envrironmentArgument.targetConfig
-  scriptKeyToInvoke = scriptKeyToInvoke || envrironmentArgument.scriptKeyToInvoke
-
-  // target application configuration file:
-  ;({ path: targetAppConfigPath } = configurationFileLookup({
-    configurationPath: targetAppConfigPath, 
-    currentDirectory,
-    configurationBasePath: ownConfiguration.targetApp.configurationBasePath
-  }))
-  // assret entrypoint configuration objects/options exist.
-  console.assert(require.resolve(targetAppConfigPath), '\x1b[41m%s\x1b[0m', `❌ Configuration file doesn't exist in ${targetAppConfigPath}`)
-  
   let contextEnvironment = vm.createContext(Object.assign(
     global,
     {
       _requiredModule_: async (...args) => { // similar to a curry function wrapper, setting default values
         // process args setting default values
-        args[0] = Object.assign({
-          targetAppConfigPath, 
-          scriptKeyToInvoke
-        }, args[0])
-        await scriptManager(...args).catch(error => console.log(error))
+        args[0] = Object.assign(defaultEvaluateCallValueForFirstParameter, args[0]) // these are is specific number of parameters that `scriptManager` function has
+        await callback(...args).catch(error => console.log(error))
       }, 
       
     }
