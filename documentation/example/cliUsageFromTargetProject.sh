68fa5487a43e8ddd98081556091581a18bd5bec5
Safi <nassarsafi@gmail.com>
2020-01-22 20:41:01 +0200

    🏗️ Build distribution code.

--- documentation/example/cliUsageFromTargetProject.sh
+++
@@ -1,5 +0,0 @@
-# Using `scriptManager` from inside a target project that would have it installed
-# relative to project`s root.
-node ./node_modules/@dependency/appDeploymentManager/source/entrypoint/cliAppManager/transpilation.entrypoint.js scriptKeyToInvoke=moduleExample
-# OR
-yarn run scriptManager scriptKeyToInvoke=moduleExample
